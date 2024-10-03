298a3a6f242dbc66defba443fe5b5eb9cffb60d3
minivan <7175380@qq.com>
2024-10-04 04:10:01 +0800

    fix: fix new main window bug

--- run.py
+++ run.py
@@ -31,9 +31,6 @@ from pyzotero import zotero
 
 from pynput import keyboard
 
-if sys.platform == 'darwin':
-    from AppKit import NSApp, NSApplication, NSApplicationActivationPolicyAccessory
-
 def resource_path(relative_path):
     if getattr(sys, 'frozen', False):
         base_path = os.path.join(os.path.dirname(sys.executable), 'Resources')
@@ -1219,10 +1216,6 @@ class MainWindow(QWidget):
 # --- Main Entry Point ---
 def main():
     app = QApplication(sys.argv)
-    
-    if sys.platform == 'darwin':
-        NSApp.setActivationPolicy_(NSApplicationActivationPolicyAccessory)
-    
     app.setQuitOnLastWindowClosed(False) 
     window = MainWindow()
     window.show()
